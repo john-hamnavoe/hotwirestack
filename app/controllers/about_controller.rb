7b5668555debb63977471615de115e188315d1fc
john-hamnavoe <john.wallace@hamnavoepartners.com>
2024-10-16 16:10:04 +0100

    debug test on the about controller

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,6 @@
 class AboutController < ApplicationController
   def index
+    @user = Current.user
+    @index_views = @user.index_views
   end
 end
--- test/controllers/about_controller_test.rb
+++ test/controllers/about_controller_test.rb
@@ -4,5 +4,8 @@ class AboutControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get about_index_url
     assert_response :success
+    assert_equal User.where(logged_in: true).first, assigns(:user)
+    index_views = assigns(:index_views)
+    assert_equal 2, index_views.count
   end
 end
