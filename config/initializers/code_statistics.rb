60f77d70178201717a8ef1f4fc022cf097dad4fc
john-hamnavoe <john.wallace@hamnavoepartners.com>
2024-10-22 18:41:02 +0100

    fix failing test, remove code_stastics as view_component updated

--- config/initializers/code_statistics.rb
+++
@@ -1,4 +0,0 @@
-# PR has been done on view_component to register the view components directory
-# but it's not released yet
-require "rails/code_statistics"
-Rails::CodeStatistics.register_directory("ViewComponent tests", "test/components", test_directory: true)
